8801505c30c0aca37f4ca5c57c5e2b6fc6b6c3b1
imliuxi <1315998513@qq.com>
2021-12-01 00:14:30 +0800

    [modify] 学习与练习

--- itstack-demo-design-3-02/src/test/java/org/itstack/demo/design/test/ApiTest.java
+++ itstack-demo-design-3-02/src/test/java/org/itstack/demo/design/test/ApiTest.java
@@ -8,15 +8,20 @@ public class ApiTest {
     @Test
     public void test_Builder(){
         Builder builder = new Builder();
+        double one = 132.52D;
+        System.out.println(builder.levelOne(one).getDetail());
+
+
+//        double two = 98.25D;
+//        double third = 85.43D;
 
         // 豪华欧式
-        System.out.println(builder.levelOne(132.52D).getDetail());
 
         // 轻奢田园
-        System.out.println(builder.levelTwo(98.25D).getDetail());
-
-        // 现代简约
-        System.out.println(builder.levelThree(85.43D).getDetail());
+//        System.out.println(builder.levelTwo(two).getDetail());
+//
+//        // 现代简约
+//        System.out.println(builder.levelThree(third).getDetail());
     }
 
 }
